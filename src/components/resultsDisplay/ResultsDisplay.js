61046e60f786904d98e91221edf6c4e9d5225350
ademonfa <121231492+alicedmf@users.noreply.github.com>
2022-12-31 17:01:25 +0100

    problems fixed

--- src/components/resultsDisplay/ResultsDisplay.js
+++ src/components/resultsDisplay/ResultsDisplay.js
@@ -2,29 +2,29 @@ import React from 'react';
 import ImageList from '@material-ui/core/ImageList';
 import { ImageListItem } from '@material-ui/core';
 
-function ResultsDisplay(pics){
-    const imageListContent=(<ImageList cols={4}>
-        {pics.map(img => (
-            <ImageListItem> key={img.id}
-            <img src={img.largeImageURL} alt="" />
-            </ImageListItem>
-    
-            ))}
-        </ImageList>) 
+function ResultsDisplay(pics) {
+    let imageListContent = null;
 
-    if (imageListContent==null){
-        <div>
-            <h2>Aucun resultat</h2>
-        </div>
-
+    if (pics.length == 0) {
+        imageListContent = (
+            <div>
+                <h2>Aucun resultat</h2>
+            </div>
+        )
     } else {
-        <div>
-            {imageListContent}
-        </div>
-    }
+        imageListContent = (
+                <ImageList cols={4}>
+                    {pics.map(img => (
+                        <ImageListItem> key={img.id}
+                            <img src={img.largeImageURL} alt="" />
+                        </ImageListItem>
 
-    
+                    ))}
+                </ImageList>
+        )
+    }
+    return imageListContent;
 }
 
-    
+
 export default ResultsDisplay;
--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -4,45 +4,33 @@ import ImageList from '@material-ui/core/ImageList';
 import ResultsDisplay from '../resultsDisplay/ResultsDisplay';
 import { useState } from 'react';
 
-function SearchBar () {
+function SearchBar() {
     const [query, setQuery] = useState("");
     const [pics, setPics] = useState([]);
     const searchImages = async (e) => {
         e.preventDefault();
-        console.log("Submitting the Form")
         var API_KEY = '32265864-051f1f4a77558a7656ff80c35';
-        var url= `https://pixabay.com/api/?key=${API_KEY}&q=${query}`;
-        console.log(url);
-        axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${query}`).then(response => {const results= response.data.hits; setPics(results);});
-        console.log(pics);
-         
-                    
-    
-        
+        var url = `https://pixabay.com/api/?key=${API_KEY}&q=${query}`;
+        if(query != "") {
+            axios.get(url).then(response => { const results = response.data.hits; setPics(results); });
+        } else {
+            setPics([]);
+        }
     };
-    
-    ResultsDisplay (pics);
-
-    console.log(query);
     return (
-        <form className="form" onSubmit={searchImages}>
-            <div style={{padding: '50px'}}>
-                <input type="text" name="searchText" 
-                    className="input" 
-                    value={query} 
-                    onChange={(e) => setQuery(e.target.value)}/>
-                
-                <button type="submit" > Search </button>
-                
-            </div>
-        </form>
-        
-        
-            
+        <div>
+            <form className="form" onSubmit={searchImages}>
+                <div style={{ padding: '50px' }}>
+                    <input type="text" name="searchText"
+                        className="input"
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)} />
+                    <button type="submit" > Search </button>
+                </div>
+            </form>
+            {ResultsDisplay(pics)}
+        </div>
     )
-
-
-    
 }
 
 
